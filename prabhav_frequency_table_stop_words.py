3f17ea96fabfcfe8a4f1e4270a4e405316a8e1d3
psparipati <114347574+psparipati@users.noreply.github.com>
2024-05-22 15:23:51 -0500

    Update prabhav_frequency_table_stop_words.py

--- prabhav_frequency_table_stop_words.py
+++ prabhav_frequency_table_stop_words.py
@@ -6,7 +6,20 @@ beta = open("stop_words.txt", "r")
 alpha = beta.read()
 stop_words = alpha.split(", ")
 
-abcd_ids = [711, 733, 523, 542, 731, 763, 669, 739, 690, 742, 724, 112, 668, 714, 671, 32, 722, 434, 713, 751, 760, 102, 654, 695, 762, 470, 33, 432, 429, 265, 673, 111, 588, 734, 692, 477, 518, 720, 445, 437, 752, 206, 670, 544, 491, 737, 685, 492, 53, 676, 575, 469, 183, 717, 665, 715, 410, 425, 430, 586, 769, 235, 462, 741, 524, 718, 758, 465, 745, 520, 431, 721, 171, 484, 759, 672, 433, 749, 502, 756, 747, 757, 26, 765, 667]
+abcd_ids = [26, 27, 28, 29, 30, 31, 32, 33, 39, 50, 52, 53, 101, 102, 110, 111, 112, 114, 115, 116, 117, 119, 151,
+            171, 183, 188, 196, 201, 206, 235, 265, 275, 276, 306, 313, 314, 316, 317, 318, 319, 320, 321, 322, 323,
+            324, 325, 326, 327, 328, 329, 330, 356, 401, 405, 406, 407, 409, 410, 411, 412, 413, 414, 415, 418, 419,
+            420, 422, 423, 424, 425, 426, 427, 428, 429, 430, 431, 432, 433, 434, 435, 436, 437, 438, 439, 440, 441,
+            442, 443, 444, 445, 462, 463, 464, 465, 466, 467, 468, 469, 470, 471, 472, 473, 474, 475, 476, 477, 478,
+            483, 484, 491, 492, 493, 502, 506, 518, 520, 523, 524, 525, 534, 538, 542, 544, 547, 548, 549, 560, 568,
+            574, 575, 577, 578, 581, 582, 583, 586, 588, 601, 605, 611, 618, 620, 626, 631, 632, 654, 655, 658, 659,
+            660, 662, 663, 664, 665, 666, 667, 668, 669, 670, 671, 672, 673, 674, 676, 677, 678, 679, 680, 681, 682,
+            683, 684, 685, 686, 687, 688, 689, 690, 691, 692, 693, 694, 695, 696, 697, 698, 699, 700, 701, 702, 703,
+            704, 706, 709, 710, 711, 712, 713, 714, 715, 716, 717, 718, 719, 720, 721, 722, 723, 724, 725, 726, 728,
+            729, 730, 731, 732, 733, 734, 735, 736, 737, 738, 739, 740, 741, 742, 743, 744, 745, 746, 747, 748, 749,
+            750, 751, 752, 753, 754, 755, 756, 757, 758, 759, 760, 761, 762, 763, 764, 765, 766, 767, 768, 769, 770,
+            771, 772]
+
 
 def create_frequency_table(id, identify_stop_words):
     response = requests.get(f'https://abcd2.projectabcd.com/api/getinfo.php?id={id}', headers={"User-Agent": "XY"})
@@ -20,7 +33,7 @@ def create_frequency_table(id, identify_stop_words):
             if word in stop_words:
                 continue
             if word not in description_dictionary:
-                description_dictionary.update({word:1})
+                description_dictionary.update({word: 1})
             elif word in description_dictionary:
                 v = description_dictionary[word]
                 v = v + 1
@@ -33,7 +46,7 @@ def create_frequency_table(id, identify_stop_words):
     if identify_stop_words == False:
         for word in description_list:
             if word not in description_dictionary:
-                description_dictionary.update({word:1})
+                description_dictionary.update({word: 1})
             elif word in description_dictionary:
                 v = description_dictionary[word]
                 v = v + 1
@@ -43,5 +56,6 @@ def create_frequency_table(id, identify_stop_words):
         print("\n# | Word\n--|----------------")
         for key in description_dictionary:
             print(f'{description_dictionary[key]} | {key}')
+
 
 create_frequency_table(714, True)
